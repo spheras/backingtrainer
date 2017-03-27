62cf52b07cf036e19c8eda7075c832154396efd5
spheras <joseamuedo@gmail.com>
2017-03-27 06:22:14 +0200

    some basic stuff: internationalization basic page structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,40 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TranslateService } from 'ng2-translate';
+import { AlertController } from 'ionic-angular';
 
 import { MainPage } from '../pages/main/main.component';
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.component.html'
 })
+
+/**
+ * @class
+ * @name MyApp
+ * @description Application Class to start everything
+ */
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
+  /**
+   * To set the root page
+   */
   rootPage: any = MainPage;
-  pages: Array<{ title: string, component: any }>;
 
-
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,
+    private translate: TranslateService, private alertCtrl: AlertController) {
+    // this language will be used as a fallback when a translation isn't found in the current language
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Page One', component: MainPage },
-      { title: 'Page Two', component: MainPage }
-    ];
   }
 
-  initializeApp() {
+  /**
+   * @name initializeApp
+   * @description initialize the application
+   */
+  private initializeApp() {
+    this.initializeLocale();
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -34,10 +44,46 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
+  private initializeLocale() {
+    var userLang = navigator.language.split('-')[0]; // use navigator lang if available
+    this.translate.setDefaultLang('en');
+    this.translate.use(userLang);
+  }
+
+  /**
+   * @name openMain
+   * @description Open the Main Screen
+   */
+  openMain() {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    this.nav.setRoot(MainPage);
+  }
+
+  /**
+   * @name openSettings
+   * @description Open the Settings Screen
+   */
+  openSettings() {
+    let alert = this.alertCtrl.create({
+      title: 'TODO!',
+      subTitle: 'This section is pending!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  /**
+   * @name openTunner
+   * @description open the Tunner Screen
+   */
+  openTunner() {
+    let alert = this.alertCtrl.create({
+      title: 'TODO!',
+      subTitle: 'This section is pending!',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
---
+++ src/pages/player/player.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'player.component.html',
+    styleUrls: ['player.component.css']
+})
+
+/**
+ * @class
+ * @name PlayerPage
+ * @description Page to play a backing track
+ */
+export class PlayerPage {
+
+    constructor() {
+
+    }
+
+
+}
