e216f190903612556a70f997462636bf95b9e4e0
spheras <joseamuedo@gmail.com>
2017-04-27 17:43:46 +0200

    new version

--- assets/soundfonts/metronome-wav.js
+++ assets/soundfonts/metronome-wav.js
@@ -2,5 +2,6 @@
 if (typeof(MIDI) === 'undefined') var MIDI = {};
 if (typeof(MIDI.Soundfont) === 'undefined') MIDI.Soundfont = {};
 MIDI.Soundfont.metronome = {
-"A0": "assets/soundfonts/metronome-wav/high.wav"
+"A0": "assets/soundfonts/metronome-wav/tone.wav",
+"Bb0": "assets/soundfonts/metronome-wav/low.wav"
 }
