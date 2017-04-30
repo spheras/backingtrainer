fb5f0fc2951525e35f048a71554d00e58682e3a1
spheras <joseamuedo@gmail.com>
2017-04-30 17:30:51 +0200

    reseting

--- assets/soundfonts/metronome-wav.js
+++
@@ -1,7 +0,0 @@
-
-if (typeof(MIDI) === 'undefined') var MIDI = {};
-if (typeof(MIDI.Soundfont) === 'undefined') MIDI.Soundfont = {};
-MIDI.Soundfont.metronome = {
-"A0": "assets/soundfonts/metronome-wav/tone.wav",
-"Bb0": "assets/soundfonts/metronome-wav/low.wav"
-}
